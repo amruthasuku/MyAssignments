f1890dcfa30a300c4f7a5b02b7d683e14434d00c
amruthasuku <124479236+amruthasuku@users.noreply.github.com>
2023-02-05 11:46:11 +0800

    Add files via upload

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class HelloSelenium {
+	public static void main(String[] args) {
+		
+		int i = 5;
+		boolean v = true;
+		String n = "STring";	
+		System.out.println(i);
+		System.out.println(n);
+		System.out.println(v);
+		
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 4;
+		short noOfMirrors = 2;
+		long chassisNumber = 23456784l;
+		boolean isPunctured = true ;
+		String bikeName = "Yamaha";
+		double runningKM = 1573.5;
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+		
+		
+		
+	}
+}
