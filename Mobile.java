6c9972a14bbda3b37d26d3b5118f70dfe3fcd519
amruthasuku <124479236+amruthasuku@users.noreply.github.com>
2023-02-05 11:28:01 +0800

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		
+		String mobileBrandName = "Sony";
+		char mobileLogo ='@';
+		short noOfMobilePiece = 234;
+		int modelNumber = 222;
+		long mobileImeiNumber = 2345672345l;
+		float mobilePrice = 23000 ;
+		boolean isDamaged = false;
+		System.out.println(mobileBrandName);
+		System.out.println(mobileLogo);
+		System.out.println(noOfMobilePiece);
+		System.out.println(mobileImeiNumber);
+		System.out.println(mobilePrice);
+		System.out.println(isDamaged);
+		
+	}
+
+}
